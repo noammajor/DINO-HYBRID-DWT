edf8568570c227d40b48adacc2639714a98e82e0
noammajor <noam.major@campus.technion.ac.il>
2026-06-05 12:07:56 +0200

    config

--- TSDiNO/config.py
+++ TSDiNO/config.py
@@ -175,9 +175,9 @@ config = {
 
     # ── Downstream: Forecasting ───────────────────────────────────────────────
     "pred_len": 96,
-    "epochs_forecasting": 20,
-    "lr_forecasting": 2e-4,
-    "min_lr_forecasting": 2e-5,
+    "epochs_forecasting": 50,
+    "lr_forecasting": 2e-5,
+    "min_lr_forecasting": 2e-6,
     "batch_size_forecast": 128,
     "parms_for_training_forecasting": ['HUFL', 'HULL', 'MUFL', 'MULL', 'LUFL', 'LULL', 'OT'],
     "parms_for_testing_forecasting":  ['HUFL', 'HULL', 'MUFL', 'MULL', 'LUFL', 'LULL', 'OT'],
