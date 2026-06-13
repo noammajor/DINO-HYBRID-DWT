c1de1f81c9cbf47e3cf6a612491f8d420fcb29c2
noammajor <noam.major@campus.technion.ac.il>
2026-06-13 10:52:18 +0200

    LR's

--- TSDiNO/config.py
+++ TSDiNO/config.py
@@ -53,7 +53,7 @@ config = {
 
     # ── Optimizer ─────────────────────────────────────────────────────────────
     "optimizer": "adamw",           # "adamw" | "sgd"
-    "lr": 0.001,
+    "lr": 5e-4,
     "min_lr": 1e-6,
     "warmup_epochs": 1,
     "weight_decay": 0.04,
